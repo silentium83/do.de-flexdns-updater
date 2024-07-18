f768f60e2f1e147371bdad6f6f548cadaf150c77
Silentium <12755340+silentium83@users.noreply.github.com>
2024-07-18 15:53:43 +0200

    add ip6lanprefix parameter

--- updater.sh
+++ updater.sh
@@ -10,7 +10,9 @@ fi
 
 while [ : ]
 do
-  curl -s --user $USERNAME:$PASSWORD https://ddns.do.de?myip=$(curl -s https://api.ipify.org)
+  IPV4=$(curl -s https://api.ipify.org)
+  IPV6PREFIX=$(ip -6 addr|awk '{print $2}'|grep -P '^(?!fe80)[[:alnum:]]{4}:.*/64'|head -n1|rev|cut -c23-|rev)
+  curl -s --user $USERNAME:$PASSWORD "https://ddns.do.de?myip=${IPV4}&ip6lanprefix=${IPV6PREFIX}:/64"
   echo 
   sleep $INTERVAL
 done
