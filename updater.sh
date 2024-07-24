dd3a3046181f18fbe2c30ec940e4d3dadd3a89f3
Silentium <12755340+silentium83@users.noreply.github.com>
2024-07-24 03:37:44 +0200

    undo debug changes

--- updater.sh
+++ updater.sh
@@ -15,29 +15,26 @@ if ! $SET_IPV4 && ! $SET_IPV6PREFIX; then
   exit 2
 fi
 
-# while [ : ]
-# do
+while [ : ]
+do
   URL="https://ddns.do.de?"
   UPDATE=false
   if $SET_IPV4; then
     IPV4=$(curl -s https://api.ipify.org)
-    echo "IPV4=${IPV4}   ${DATA}/ipv4=$(<"${DATA}"/ipv4)"
     URL="${URL}myip=${IPV4}&"
     [ ! -e "${DATA}/ipv4" ] || [ "${IPV4}" != "$(<"${DATA}"/ipv4)" ] && UPDATE=true
   fi
   if $SET_IPV6PREFIX; then
     IPV6PREFIX=$(ip -6 addr|awk '{print $2}'|grep -P '^(?!fe80)[[:alnum:]]{4}:.*/64'|head -n1|rev|cut -c23-|rev)
-    echo "IPV6PREFIX=${IPV6PREFIX}   ${DATA}/ipv6prefix=$(<"${DATA}"/ipv6prefix)"
     URL="${URL}ip6lanprefix=${IPV6PREFIX}:/64"
     [ ! -e "${DATA}/ipv6prefix" ] || [ "${IPV6PREFIX}" != "$(<"${DATA}"/ipv6prefix)" ] && UPDATE=true
   fi
   if $UPDATE || [[ $(date +%s -r "${DATA}"/updated 2>/dev/null) -lt $(date +%s --date="25 days ago") ]]; then
-    # if curl -s --user $USERNAME:$PASSWORD "${URL}"; then
-    if echo curl -s --user $USERNAME:$PASSWORD "${URL}"; then
+    if curl -s --user $USERNAME:$PASSWORD "${URL}"; then
       touch "${DATA}/updated"
       echo "${IPV4}" > "${DATA}/ipv4"
       echo "${IPV6PREFIX}" > "${DATA}/ipv6prefix"
     fi
   fi
-#   sleep $INTERVAL
-# done
+  sleep $INTERVAL
+done
